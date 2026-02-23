ffa2e26c03b6320a46949646dd7d9cc5dbedb0b3
beactony23 <beactony23@gmail.com>
2026-02-23 10:31:44 -0600

    Implement '/respuestas' route for database queries
    Added a new route '/respuestas' to fetch responses from the database.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/respuestas')
+def respuestas():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+      host="46.28.42.226",
+      user="u760464709_24005037_usr",
+      password="N&2lbK=8;Mrt",
+      database="u760464709_24005037_bd"
+    )
+    mycursor = mydb.cursor(
+    mycursor.execute("SELECT * FROM Respuesta")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
